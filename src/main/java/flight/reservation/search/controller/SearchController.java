60de3b961268cac94c405be4e4989990af9de9ee
JYKANG <xadamantiumx@naver.com>
2024-06-08 17:52:09 +0900

    2주차_조회 화면 및 컨트롤러 연결

---
+++ src/main/java/flight/reservation/search/controller/SearchController.java
@@ -0,0 +1,13 @@
+package flight.reservation.search.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+    @RequestMapping("/search")
+    public String test(){
+
+        return "search";
+    }
+}
